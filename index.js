79a984b351ca3e4fcd2fb4a3d23a70a4605fc94e
deenoloo <doreenoloo2@gmail.com>
2025-03-10 11:00:59 -0400

    array-filter-method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, fooy) {
+    return drivers.filter(driver => 
+      driver.toLowerCase() === fooy.toLowerCase()
+    );
+}
+function fuzzyMatch(drivers, fooy) {
+    return drivers.filter(driver =>
+      driver.startsWith(fooy)
+    );
+  }
+  function matchName(drivers, fooy) {
+    return drivers.filter(driver =>
+      driver.name === fooy
+    );
+  }
